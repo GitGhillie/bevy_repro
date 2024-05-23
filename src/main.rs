5a0ff0e98369c920c51e5377554225bc0d018833
GitGhillie <jillisnoordhoek@gmail.com>
2024-05-23 13:25:58 +0200

    texture transform issue

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn((Camera3dBundle {
+        transform: Transform::from_xyz(1.0, 0.0, 20.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    },));
+
+    commands.spawn(DirectionalLightBundle {
+        directional_light: DirectionalLight {
+            shadows_enabled: true,
+            ..default()
+        },
+        ..default()
+    });
+    commands.spawn(SceneBundle {
+        scene: asset_server.load("stripped/strippedHistory.gltf#Scene0"),
+        ..default()
+    });
 }
