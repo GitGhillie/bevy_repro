81d36ef9c18f5ed519bbd47e5138ad7e65035fa7
GitGhillie <jillisnoordhoek@gmail.com>
2026-05-21 22:17:07 +0200

    Update to bevy latest

--- src/main.rs
+++ src/main.rs
@@ -8,20 +8,17 @@ fn main() {
 }
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.spawn((Camera3dBundle {
-        transform: Transform::from_xyz(1.0, 0.0, 20.0).looking_at(Vec3::ZERO, Vec3::Y),
-        ..default()
-    },));
+    commands.spawn((
+        Camera3d { ..default() },
+        Transform::from_xyz(1.0, 0.0, 20.0).looking_at(Vec3::ZERO, Vec3::Y),
+    ));
 
-    commands.spawn(DirectionalLightBundle {
-        directional_light: DirectionalLight {
-            shadows_enabled: true,
-            ..default()
-        },
+    commands.spawn(DirectionalLight {
+        shadow_maps_enabled: true,
         ..default()
     });
-    commands.spawn(SceneBundle {
-        scene: asset_server.load("scene.gltf#Scene0"),
-        ..default()
-    });
+
+    commands.spawn(WorldAssetRoot(
+        asset_server.load(GltfAssetLabel::Scene(0).from_asset("scene.gltf")),
+    ));
 }
